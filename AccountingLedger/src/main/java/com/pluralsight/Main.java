7fd2774167319dcb0788c53f7c4b758aa34cbdf6
venomgto <95735342+venomgto@users.noreply.github.com>
2024-04-30 13:46:53 -0700

    got the deposit class to work

--- AccountingLedger/src/main/java/com/pluralsight/Main.java
+++ AccountingLedger/src/main/java/com/pluralsight/Main.java
@@ -1,10 +1,16 @@
 package com.pluralsight;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Main
 {
-    private static Scanner userInput = new Scanner(System.in);
+    static Scanner userInput = new Scanner(System.in);
+
+    private static String TRANSACTION_FILE = "Transactions.csv";
 
     public static void main(String[] args)
     {
@@ -27,7 +33,47 @@ public class Main
 
         switch (choice)
         {
+            case "d":
+                addDeposit();
+                break;
+        }
+    }
 
+    private void saveAsCsv()
+    {
+        File file = new File("files/Transaction.csv");
+
+        try(FileWriter fileWriter = new FileWriter(file);
+        PrintWriter writer = new PrintWriter(fileWriter);)
+        {
+
+        }
+        catch (IOException e)
+        {
+
+        }
+
+    }
+
+    private static void addDeposit()
+    {
+        try(PrintWriter wrtiter = new PrintWriter(new FileWriter(TRANSACTION_FILE, true)))
+        {
+            System.out.println("-------------------------------------");
+            System.out.println("Welcome to Plural deposit ");
+            System.out.print("Date (yyyy-MM-dd): ");
+            String date = userInput.nextLine();
+            System.out.print("Time (HH:mm:ss): )");
+            String time = userInput.nextLine();
+            System.out.print("Deposit amount: ");
+            double amount = userInput.nextDouble();
+            userInput.nextLine();
+
+            wrtiter.println(date + "|" + time + "|" + "|" + amount);
+            System.out.println("Deposit successful!");
+        } catch (IOException e)
+        {
+            System.out.println("Error: Failed to add deposit." );
         }
     }
 }
