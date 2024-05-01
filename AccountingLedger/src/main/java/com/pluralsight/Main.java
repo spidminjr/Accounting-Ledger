1095fdb80b29ee0dbfde4b02c81cf91ea6deb657
venomgto <95735342+venomgto@users.noreply.github.com>
2024-05-01 09:50:04 -0700

    added make payment class

--- AccountingLedger/src/main/java/com/pluralsight/Main.java
+++ AccountingLedger/src/main/java/com/pluralsight/Main.java
@@ -4,13 +4,15 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main
 {
     static Scanner userInput = new Scanner(System.in);
+    private ArrayList<Transaction> transactions;
+    private static final String TRANSACTION_FILE = "Transactions.csv";
 
-    private static String TRANSACTION_FILE = "Transactions.csv";
 
     public static void main(String[] args)
     {
@@ -36,6 +38,9 @@ public class Main
             case "d":
                 addDeposit();
                 break;
+            case "p":
+                makePayment();
+                break;
         }
     }
 
@@ -43,10 +48,14 @@ public class Main
     {
         File file = new File("files/Transaction.csv");
 
-        try(FileWriter fileWriter = new FileWriter(file);
+        try(FileWriter fileWriter = new FileWriter(file, true);
         PrintWriter writer = new PrintWriter(fileWriter);)
         {
+            for(Transaction transaction : transactions)
+            {
+                writer.printf("%s|%s|%s|%s|%.2f\n",transaction.getDate(), transaction.getTime(), transaction.getDescription(), transaction.getVendor(), transaction.getAmount());
 
+            }
         }
         catch (IOException e)
         {
@@ -57,7 +66,7 @@ public class Main
 
     private static void addDeposit()
     {
-        try(PrintWriter wrtiter = new PrintWriter(new FileWriter(TRANSACTION_FILE, true)))
+        try(PrintWriter writer = new PrintWriter(new FileWriter(TRANSACTION_FILE, true)))
         {
             System.out.println("-------------------------------------");
             System.out.println("Welcome to Plural deposit ");
@@ -69,11 +78,38 @@ public class Main
             double amount = userInput.nextDouble();
             userInput.nextLine();
 
-            wrtiter.println(date + "|" + time + "|" + "|" + amount);
+            writer.println(date + "|" + time + "|" + "|" + amount);
             System.out.println("Deposit successful!");
         } catch (IOException e)
         {
             System.out.println("Error: Failed to add deposit." );
         }
     }
+
+    private static void makePayment()
+    {
+      try(PrintWriter writer = new PrintWriter(new FileWriter(TRANSACTION_FILE), true))
+      {
+          System.out.println("-------------------------------------");
+          System.out.println("Welcome to Plural payment ");
+          System.out.print("Date (yyyy-MM-dd): ");
+          String date = userInput.nextLine();
+          System.out.print("Time (HH:mm:ss): )");
+          String time = userInput.nextLine();
+          System.out.print("Description: ");
+          String description = userInput.nextLine();
+          System.out.print("Vendor: ");
+          String vendor = userInput.nextLine();
+          System.out.print("Amount: ");
+          double amount = userInput.nextDouble();
+          userInput.nextLine();
+
+          writer.println(date + "|" + time + "|" + "|" + description + "|" + vendor + "|" + (-amount));
+          System.out.println("Payment successful!");
+      } catch(IOException e)
+      {
+          System.out.println("Error: Failed to make payment." );
+      }
+    }
+    
 }
